4abdca2ee8809ac1e59fda3a2b6f16782ffc604c
Frank <frankedwards50@gmail.com>
2024-07-12 23:54:07 -0700

    Cookie Banner

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -5,6 +5,7 @@ import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 // Consistent Across Pages
 import Navbar from "../components/general-page-layout/navbar/Navbar";
 import Footer from "../components/general-page-layout/footer/Footer";
+import CookieBanner from "../components/cookie-banner/CookieBanner";
 import ScrollToTop from "../components/general-page-layout/ScrollToTop";
 
 // Constants
@@ -17,6 +18,14 @@ export const Route = createRootRoute({
       <Navbar />
       <Outlet />
       {IS_DEV === "TRUE" && <TanStackRouterDevtools />}
+      <CookieBanner
+        bodyText="To ensure that you have the best possible experience while visiting us, we use cookies and similar technologies."
+        button1={{
+          text: "Dismiss",
+          variant: "primary-dark",
+          buttonSize: "small",
+        }}
+      />
       <Footer />
     </>
   ),
