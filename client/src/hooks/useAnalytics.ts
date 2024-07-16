f0d99fdb1f1239e13665d2cb243a49dfc4be5388
Frank <frankedwards50@gmail.com>
2024-07-15 21:43:09 -0700

    Analytics Logic Improved

--- client/src/hooks/useAnalytics.ts
+++ client/src/hooks/useAnalytics.ts
@@ -1,6 +1,7 @@
 // Library Imports
 import { useEffect, useMemo } from "react";
 // Functions, Helpers, Utils, and Hooks
+import fetchData from "../functions/network/fetchData";
 import useDeviceInfo from "./useDeviceInfo";
 
 const useAnalytics = () => {
@@ -9,15 +10,17 @@ const useAnalytics = () => {
   const sendAnalytics = useMemo(() => {
     return () => {
       const pageVisits = JSON.parse(localStorage.getItem("pageVisits") || "[]");
+
       if (pageVisits.length > 0) {
         const payload = {
           userAgentInfo,
           pageVisits,
         };
 
-        fetch("/api/analytics/update-analytics", {
+        fetchData("/api/analytics/update-analytics", {
           method: "POST",
           headers: {
+            Accept: "*/*",
             "Content-Type": "application/json",
           },
           body: JSON.stringify(payload),
@@ -29,7 +32,11 @@ const useAnalytics = () => {
   }, [userAgentInfo]);
 
   useEffect(() => {
-    const intervalId = setInterval(sendAnalytics, 5 * 60 * 1000); // Every 5 minutes
+    /* 
+      This sets an interval to send analytics every minute, to decrease the frequency,
+      change the 1 to the desired time in minutes
+    */
+    const intervalId = setInterval(sendAnalytics, 1 * 60 * 1000);
 
     window.addEventListener("beforeunload", sendAnalytics);
 
