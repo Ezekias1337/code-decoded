a784d61880fc08c720269283189d9bc87599606c
Frank Edwards <frankedwards50@gmail.com>
2025-05-02 21:53:25 -0700

    usePageTracking hook now includes initial page load

--- client/src/hooks/usePageTracking.ts
+++ client/src/hooks/usePageTracking.ts
@@ -1,29 +1,44 @@
-// Library Imports
-import { useEffect } from "react";
-import { useRouter, RouterEvent } from "@tanstack/react-router";
+import { useEffect, useCallback, useState } from "react";
+import { useRouter, useLocation } from "@tanstack/react-router";
 
 const usePageTracking = () => {
   const router = useRouter();
+  const location = useLocation();
+
+  const [hasHydrated, setHasHydrated] = useState(false);
 
   useEffect(() => {
-    const handleRouteChange = (event: RouterEvent) => {
-      const { toLocation } = event;
-      const currentVisits = JSON.parse(
-        localStorage.getItem("pageVisits") || "[]"
-      );
-      currentVisits.push({
-        path: toLocation.pathname,
-        timestamp: new Date().toISOString(),
-      });
-      localStorage.setItem("pageVisits", JSON.stringify(currentVisits));
-    };
+    setHasHydrated(true);
+  }, []);
+
+  const handleRouteChange = useCallback(() => {
+    const currentVisits = JSON.parse(
+      localStorage.getItem("pageVisits") || "[]"
+    );
+    currentVisits.push({
+      path: location.pathname,
+      timestamp: new Date().toISOString(),
+    });
+    localStorage.setItem("pageVisits", JSON.stringify(currentVisits));
+  }, [location.pathname]);
+
+  /* 
+    ! handleRouteChange is intentionally not included in the dependency array because it causes
+    ! navigations to be included twice
+  */
+  
+  useEffect(() => {
+    if (hasHydrated) {
+      handleRouteChange();
+    }
+  }, [hasHydrated]);
+
+  useEffect(() => {
+    if (!hasHydrated) return;
 
     const unsubscribe = router.subscribe("onResolved", handleRouteChange);
-
-    return () => {
-      unsubscribe();
-    };
-  }, [router]);
+    return () => unsubscribe();
+  }, [router, handleRouteChange, hasHydrated]);
 };
 
 export default usePageTracking;
